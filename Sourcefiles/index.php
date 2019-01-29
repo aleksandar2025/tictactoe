d24b9e4dc1121d1905888bd1cb8c78b7aae4f12d
Alex Ivas <goodguyfc@protonmail.com>
2019-01-29 10:09:23 +0000

    add initial form as well as some of the basic inputs and the arrangement

--- Sourcefiles/index.php
+++ Sourcefiles/index.php
@@ -6,9 +6,18 @@
             <title>XO</title>
         </head>
         <body>
-            <?php 
-            
+            <form method="POST" action="index.php">
+             <?php 
+                //   forloop to create more boxes
+              for ($id = 1; $id <= 9; $id++){      
+                // if statement to break and arrange the boxes
+                if($id === 4 or $id === 7) print "<br>";
+                // the box containing the input  
+                print "<input name ='$id' type ='text' size ='8'>";
+              };
             ?>
+            </form>
+           
             
         </body>
         
