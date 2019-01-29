84cc040bce6fb4f057bafee81420f8d6ac85f521
Alex Ivas <goodguyfc@protonmail.com>
2019-01-29 10:50:44 +0000

    add some initial value recognition as well as a conditional for the input to only be X or O

--- Sourcefiles/index.php
+++ Sourcefiles/index.php
@@ -14,6 +14,17 @@
                 if($id === 4 or $id === 7) print "<br>";
                 // the box containing the input  
                 print "<input name ='$id' type ='text' size ='8'>";
+                // value checking from inputs and printing out a value in a case it's not empty
+                if(isset($_POST['submit']) and !empty($_POST[$id])){
+                    // checking if the value is X or O
+                    if($_POST[$id] == "x" or  $_POST[$id] == "o"){
+                    //  keep the value inside the box
+                      print "value='$_POST[$id]'";  
+                    }
+                    else{
+                       $error = true; 
+                    };
+                };
               };
             ?>
             </form>
