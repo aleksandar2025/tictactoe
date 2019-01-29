d8c0648ab52061d5a99c41a3939d8a65e57ae830
Alex Ivas <goodguyfc@protonmail.com>
2019-01-29 11:32:01 +0000

    add horizontal data association and winning conditions

--- Sourcefiles/index.php
+++ Sourcefiles/index.php
@@ -8,6 +8,8 @@
         <body>
             <form method="POST" action="index.php">
              <?php 
+            //  adding error default variable
+            $error = false;
                 //   forloop to create more boxes
               for ($id = 1; $id <= 9; $id++){      
                 // if statement to break and arrange the boxes
@@ -20,6 +22,21 @@
                     if($_POST[$id] == "x" or  $_POST[$id] == "o"){
                     //  keep the value inside the box
                       print "value='$_POST[$id]'";  
+                    //   associating the fields with the user input. working with data and teaching the programm the winning conditions by using forloops
+                        // horizontal loop
+                        for($a = 1, $b = 2, $c = 3; $a <=7, $b <= 8, $c <= 9; $a+=3, $b+=3, $c+=3){
+                            // if statement to check if a winning condition is on the horizontal line
+                            if($_POST[$a] == $_POST[$b] and $_POST[$b] == $_POST[$c]){
+                                // determining the winner and printing out the winning message
+                                if($_POST[$a] == "x"){
+                                    $x_wins = true;
+                                } 
+                                // we already checked if $a is equal to $b and $c
+                                elseif($post[$a] == "o"){
+                                    $o_wins = true;
+                                }
+                            }
+                        }
                     }
                     else{
                        $error = true; 
