6efe6cfae03330f37a8ba0a7faab3c860af3ba0b
Alex Ivas <goodguyfc@protonmail.com>
2019-01-29 11:45:00 +0000

    add the logic behind the vertical and cross check; line 24 "value='$_POST[$id]'>"; add > to this line so that the html element can function

--- Sourcefiles/index.php
+++ Sourcefiles/index.php
@@ -21,11 +21,37 @@
                     // checking if the value is X or O
                     if($_POST[$id] == "x" or  $_POST[$id] == "o"){
                     //  keep the value inside the box
-                      print "value='$_POST[$id]'";  
+                      print "value='$_POST[$id]'>";  
                     //   associating the fields with the user input. working with data and teaching the programm the winning conditions by using forloops
                         // horizontal loop
                         for($a = 1, $b = 2, $c = 3; $a <=7, $b <= 8, $c <= 9; $a+=3, $b+=3, $c+=3){
                             // if statement to check if a winning condition is on the horizontal line
+                            if($_POST[$a] == $_POST[$b] and $_POST[$b] == $_POST[$c]){
+                                // determining the winner and printing out the winning message
+                                if($_POST[$a] == "x"){
+                                    $x_wins = true;
+                                } 
+                                // we already checked if $a is equal to $b and $c
+                                elseif($post[$a] == "o"){
+                                    $o_wins = true;
+                                }
+                            }
+                        }
+                        for($a = 1, $b = 4, $c = 7; $a <= 3, $b <= 6, $c <= 9; $a+=1, $b+=1, $c+=1){
+                            // if statement to check if a winning condition is on the vertical line
+                            if($_POST[$a] == $_POST[$b] and $_POST[$b] == $_POST[$c]){
+                                // determining the winner and printing out the winning message
+                                if($_POST[$a] == "x"){
+                                    $x_wins = true;
+                                } 
+                                // we already checked if $a is equal to $b and $c
+                                elseif($post[$a] == "o"){
+                                    $o_wins = true;
+                                }
+                            }
+                        }
+                        for($a = 1, $b = 5, $c = 9; $a <= 3, $b <= 5, $c <= 9; $a+=2, $b+=5, $c-=2){
+                            // if statement to check if a winning condition is on the cross line
                             if($_POST[$a] == $_POST[$b] and $_POST[$b] == $_POST[$c]){
                                 // determining the winner and printing out the winning message
                                 if($_POST[$a] == "x"){
